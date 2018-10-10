f864587688ab62ee7062bfc31d5ed4e3fec0f703
Erich Kauffman <kauffm2@stolaf.edu>
2018-10-10 14:07:33 -0500

    Create inventory list view

---
+++ src/views/ListView.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ListItem from '../components/ListItem';
+import { getListOfItems } from '../store/ItemStore'
+
+export default class ListView extends Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+			items: [],
+		};
+	}
+
+	createList = () => {
+		return this.state.items.map((item) => {
+			return(<ListItem key={item.itemId} itemId={item.itemId} title={item.title}/>);
+		});
+	}
+
+	componentDidMount(){
+		let itemsPromise = getListOfItems();
+		itemsPromise.then((res) => {
+			console.log(res);
+			this.setState({
+				items: res,
+			});
+		});
+	}
+
+	render(){
+		return(
+			<div>
+				{this.createList()}
+			</div>
+		);
+	}
+}
