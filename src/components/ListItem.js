759c485aac8d0348fffe60dc526c2fc8c957852f
Erich Kauffman <kauffm2@stolaf.edu>
2019-02-03 18:03:19 -0600

    Add buttons to listItems

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './ListItem.css';
+import editImg from '../images/EditButton.png';
+import sellImg from '../images/MoneyButton.png';
+import deleteImg from '../images/DeleteButton.png';
 
 let handleClick = (e, props) => {
 	props.onClick(props.itemId);
+}
+
+let buttonClick = (e, msg) => {
+	e.stopPropagation();
+	console.log(`The ${msg} button was pressed!`);
 }
 
 const ListItem = (props) => {
@@ -10,6 +18,9 @@ const ListItem = (props) => {
 		<div className='listItem' onClick={(e) => {return handleClick(e, props)}}>
 			<p className='itemId'>{props.itemId}</p>
 			<p className='titleText'>{props.title}</p>
+			<img className='itemButton' src={editImg} alt="edit" onClick={(e) => {return buttonClick(e, 'edit')}}/>
+			<img className='itemButton' src={sellImg} alt="sell" onClick={(e) => {return buttonClick(e, 'sell')}}/>
+			<img className='itemButton' src={deleteImg} alt="delete" onClick={(e) => {return buttonClick(e, 'delete')}}/>
 		</div>
 	)
 }
