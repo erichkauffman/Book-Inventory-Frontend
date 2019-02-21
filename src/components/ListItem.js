d1c56b910b3a41949bcf7bd0330b29c151216caf
Erich Kauffman <kauffm2@stolaf.edu>
2019-02-21 12:10:37 -0600

    Make listItem stateless

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,52 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ListItem.css';
 import editImg from '../images/EditButton.png';
 import sellImg from '../images/MoneyButton.png';
 import deleteImg from '../images/DeleteButton.png';
 
-export default class ListItem extends Component{
+const handleClick = (props) => {
+	props.onClick(props.itemId);
+}
 
-	constructor(props){
-		super(props);
-		this.state = {
-			className: 'listItem'
-		};
-	}
-
-	handleClick = () => {
-		this.props.onClick(this.props.itemId);
-	}
-
-	buttonClick = (e) => {
-		e.stopPropagation();
-		if(e.target.alt === 'edit'){
-			console.log('Edit button');
-		}else{
-			this.props.buttonClick(e, this.props.itemId);
-		}
-	}
-
-	componentWillReceiveProps(nextProps){
-		if(nextProps.selectedItem && nextProps.selectedItem === this.props.itemId){
-			this.setState({
-				className: 'listItemSelected'
-			});
-		}else{
-			this.setState({
-				className: 'listItem'
-			});
-		}
-	}
-
-	render(){
-		return(
-			<div className={this.state.className} onClick={this.handleClick}>
-				<p className='itemId'>{this.props.itemId}</p>
-				<p className='titleText'>{this.props.title}</p>
-				<img className='itemButton' src={editImg} alt='edit' onClick={this.buttonClick}/>
-				<img className='itemButton' src={sellImg} alt='sell' onClick={this.buttonClick}/>
-				<img className='itemButton' src={deleteImg} alt='delete' onClick={this.buttonClick}/>
-			</div>
-		)
+const buttonClick = (e, props) => {
+	e.stopPropagation();
+	if(e.target.alt === 'edit'){
+		console.log('Edit button');
+	}else{
+		props.buttonClick(e, props.itemId);
 	}
 }
+
+const ListItem = (props) => {
+	return(
+		<div className={props.selectedItem===props.itemId?'listItemSelected':'listItem'} onClick={() => {handleClick(props)}}>
+			<p className='itemId'>{props.itemId}</p>
+			<p className='titleText'>{props.title}</p>
+			<img className='itemButton' src={editImg} alt='edit' onClick={(e) => {buttonClick(e, props)}}/>
+			<img className='itemButton' src={sellImg} alt='sell' onClick={(e) => {buttonClick(e, props)}}/>
+			<img className='itemButton' src={deleteImg} alt='delete' onClick={(e) => {buttonClick(e, props)}}/>
+		</div>
+	)
+}
+
+export default ListItem;
