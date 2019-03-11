c11f5b1b451170a6ea87015282e537324a0ffac5
Erich Kauffman <kauffm2@stolaf.edu>
2019-03-10 19:29:08 -0500

    Receive data from props

--- src/views/ListView.js
+++ src/views/ListView.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ListItem from '../components/ListItem';
 import ItemDetail from '../components/ItemDetail';
 import Selector from '../components/Selector';
-import { getSellableInventory, commitRemoveAction } from '../lib/ItemRoutes';
+import { commitRemoveAction, getItemById } from '../lib/ItemRoutes';
 import './ListView.css';
 
 export default class ListView extends Component{
@@ -10,7 +10,6 @@ export default class ListView extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			items: [],
 			itemDetailRender: null,
 			id: null,
 			filter: 'itemId',
@@ -19,28 +18,30 @@ export default class ListView extends Component{
 	}
 
 	setItem = (itemId) => {
-		let item = this.state.items.find((x) => {return x.itemId === itemId});
-		this.setState({
-			itemDetailRender: item,
-			id: itemId
-		});
-	}
-
-	removeItem = (itemId) => {
-		let items = this.state.items.filter((x) => {return x.itemId !== itemId});
-		this.setState({
-			items: items
-		});
+		if(this.state.id !== itemId){
+			let itemPromise = getItemById(itemId, this.props.type);
+			itemPromise.then((raw) => {
+				let item = {...raw, ...raw.item};
+				delete item.item;
+				return item;
+			})
+			.then((item) => {
+				this.setState({
+					itemDetailRender: item,
+					id: itemId
+				});
+			});
+		}
 	}
 
 	handleRemove = (e, itemId) => {
 		if(window.confirm(`${e.target.alt}\nThis action is permanent and cannot be undone`)){
 			if(e.target.alt === 'sell'){
 				commitRemoveAction(itemId, 1);
-				this.removeItem(itemId);
+				this.props.removeItem(itemId, this.props.type);
 			}else if(e.target.alt === 'delete'){
 				commitRemoveAction(itemId, 0);
-				this.removeItem(itemId);
+				this.props.removeItem(itemId, this.props.type);
 			}else{
 				console.log("ListItem buttonClick called without proper handling");
 			}
@@ -62,7 +63,7 @@ export default class ListView extends Component{
 	}
 
 	createList = () => {
-		let items = this.state.items;
+		let items = this.props.items;
 		if(this.state.search){
 			items = items.filter((item) => {
 				return item[this.state.filter].toString().toLowerCase().includes(this.state.search.toLowerCase());
@@ -86,37 +87,13 @@ export default class ListView extends Component{
 		}
 	}
 
-	getInventory = (type) => {
-		let itemsPromise = getSellableInventory(type);
-		itemsPromise.then((res) =>{
-			if(res.length !== 0 && res[0].item){
-				return res.map((raw) => {
-					let item = {...raw, ...raw.item};
-					delete item.item;
-					return item;
-				});
-			}
-			return res;
-		})
-		.then((items) => {
-			this.setState({
-				items: items
-			});
-		});	
-	}
-
-	componentDidMount(){
-		this.getInventory(this.props.type);
-	}
-
-	componentWillReceiveProps(nextProps){
+	componentWillReceiveProps(){
 		this.setState({
 			filter: 'itemId',
 			itemDetailRender: null,
 			id: null,
 			search: ''
 		});
-		this.getInventory(nextProps.type);
 	}
 
 	render(){
