bf57fa4a0c61712bca1da253fc3170b8cd124810
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-09 13:40:07 -0500

    Add conditional render to dialog box

--- src/views/ListView/components/Dialog.js
+++ src/views/ListView/components/Dialog.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Dialog = (props) => {
-	if(!props.renderCondition){
-		return null;
-	}
-	return(
-		<div className='backDrop'>
-			<div className='dialogBox'>
-				{props.children}
+	if(props.renderCondition){
+		return(
+			<div className='backDrop'>
+				<div className='dialogBox'>
+					{props.children}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
+	return null;
 }
 
 export default Dialog;
