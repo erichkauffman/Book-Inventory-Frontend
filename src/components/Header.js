e819c2e3678fa9814c918604a7ac5c536bd06ece
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-11 08:06:43 -0500

    Add external link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import MenuLink from './MenuLink';
+import Conditional from './Conditional';
+import { apiPath } from '../config';
 
 import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
 	return(
 		<div className='header'>
 			<MenuLink link='/form/item'>New Item</MenuLink>
@@ -11,6 +13,13 @@ const Header = () => {
 			<MenuLink link='/list/items'>All Items</MenuLink>
 			<MenuLink link='/list/books'>Books</MenuLink>
 			<MenuLink link='/data'>Saved Data</MenuLink>
+			<Conditional render={props.type && props.view !== 'form'}>
+				<a href={`${apiPath}/${props.type}/csv/`}>
+					<div className='externalLinkContainer'>
+						<p>Download CSV</p>
+					</div>
+				</a>
+			</Conditional>
 		</div>
 	);
 }
