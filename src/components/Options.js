77d4f7a26d25daa260addced8df4b79896bc2727
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-11 08:54:57 -0500

    Use conditional with option in form

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
 const Options = (props) => {
-	if(props.options){
-		let ops = props.options.map((option) => {
-			return <option key={option} value={option}>{option}</option>
-		});
-		if(props.placeholder){
-			ops.push(<option key='empty' value='' disabled>Select</option>);
-		}
-		return ops;
+	let ops = props.options.map((option) => {
+		return <option key={option} value={option}>{option}</option>
+	});
+	if(props.placeholder){
+		ops.push(<option key='empty' value='' disabled>Select</option>);
 	}
-	return null;
+	return ops;
 }
 
 export default Options;
