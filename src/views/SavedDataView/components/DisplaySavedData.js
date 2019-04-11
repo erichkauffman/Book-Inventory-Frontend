66a3798637bb64d7c0245c43e5d245a18d38bce4
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-11 09:10:26 -0500

    Add conditional to display saved data

--- src/views/SavedDataView/components/DisplaySavedData.js
+++ src/views/SavedDataView/components/DisplaySavedData.js
@@ -4,17 +4,14 @@ import deleteImg from '../../../images/DeleteButton.png';
 import './DisplaySavedData.css';
 
 const DisplaySavedData = (props) => {
-	if(props.data[props.type]){
-		return props.data[props.type].map((info) => {
-			return(
-				<div key={info} className='savedDataDiv'>
-					<p>{info}</p>
-					<img src={deleteImg} alt='delete' onClick={()=>{props.deleteData(props.type, info)}}/>
-				</div>
-			);
-		});
-	}
-	return null;
+	return props.data[props.type].map((info) => {
+		return(
+			<div key={info} className='savedDataDiv'>
+				<p>{info}</p>
+				<img src={deleteImg} alt='delete' onClick={()=>{props.deleteData(props.type, info)}}/>
+			</div>
+		);
+	});
 }
 
 export default DisplaySavedData;
