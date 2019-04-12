cf14d95a806d2902c77833a6ccba7f9b3eca9215
Erich Kauffman <kauffm2@stolaf.edu>
2019-04-12 14:30:49 -0500

    Remove true indecies

--- src/views/FormView/lib/trueIndecies.js
+++
@@ -1,9 +0,0 @@
-export const trueIndecies = (booleanArray) => {
-	let newArr = [];
-	booleanArray.forEach((element, index) => {
-		if(element){
-			newArr.push(index);
-		}
-	});
-	return newArr;
-}
