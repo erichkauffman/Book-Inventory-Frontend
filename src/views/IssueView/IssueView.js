b2506c5db8daa736b48f75bb27eb3ea45b16c77c
Erich Kauffman <kauffm2@stolaf.edu>
2019-06-05 20:31:29 -0500

    Create constructor

--- src/views/IssueView/IssueView.js
+++ src/views/IssueView/IssueView.js
@@ -4,19 +4,29 @@ import Options from '../../components/Options';
 import './IssueView.css';
 
 export default class IssueView extends Component{
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			type: 0,
+			title: '',
+			message: ''
+		};
+	}
+
 	render(){
 		return(
 			<div>
 				<form>
-					<select className='issueType'>
+					<select className='issueType' onChange={(e)=>{this.setState({type:e.target.options.selectedIndex})}}>
 						<Options options={['Feature Request', 'Bug Report']}/>
 					</select>
 					<br/>
 					<label>Title:</label>
-					<input type='text' className='issueTitle'/>
+					<input type='text' className='issueTitle' onChange={(e)=>{this.setState({title:e.target.value})}}/>
 					<br/>
 					<label>Description:</label>
-					<textarea className='issueDescription'/>
+					<textarea className='issueDescription' onChange={(e)=>{this.setState({message:e.target.value})}}/>
 				</form>
 			</div>
 		);
