19f264465ec5a7e7246c108eb53504ac09a4e1b7
Erich Kauffman <kauffm2@stolaf.edu>
2019-06-09 17:07:12 -0500

    Show end screen when issue is submitted

--- src/views/IssueView/IssueView.js
+++ src/views/IssueView/IssueView.js
@@ -16,24 +16,40 @@ export default class IssueView extends Component{
 		};
 	}
 
+	handleSubmit = () => {
+		sendIssue({type: this.state.type, title: this.state.title, message: this.state.message});
+		this.setState({endScreen: true});
+	}
+
 	render(){
-		return(
-			<div>
-				<form>
-					<select className='issueType' onChange={(e)=>{this.setState({type:e.target.options.selectedIndex})}}>
-						<Options options={['Feature Request', 'Bug Report']}/>
-					</select>
-					<br/>
-					<label>Title:</label>
-					<input type='text' className='issueTitle' onChange={(e)=>{this.setState({title:e.target.value})}}/>
-					<br/>
-					<label>Description:</label>
-					<textarea className='issueDescription' onChange={(e)=>{this.setState({message:e.target.value})}}/>
-				</form>
-				<div className={checkFields(this.state, ['title', 'message'])?'submitIssue':'issueDisabled'} onClick={(e) => {sendIssue(this.state)}}>
-					<p>Submit</p>
+		if(this.state.endScreen){
+			return(
+				<div>
+					<p>Thank you for sending your issue. We will look into it shortly.</p>
+					<div className='newIssue' onClick={()=>{this.setState({type:0, title:'', message:'', endScreen:false})}}>
+						<p>New Issue</p>
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}else{
+			return(
+				<div>
+					<form>
+						<select className='issueType' onChange={(e)=>{this.setState({type:e.target.options.selectedIndex})}}>
+							<Options options={['Feature Request', 'Bug Report']}/>
+						</select>
+						<br/>
+						<label>Title:</label>
+						<input type='text' className='issueTitle' onChange={(e)=>{this.setState({title:e.target.value})}}/>
+						<br/>
+						<label>Description:</label>
+						<textarea className='issueDescription' onChange={(e)=>{this.setState({message:e.target.value})}}/>
+					</form>
+					<div className={checkFields(this.state, ['title', 'message'])?'submitIssue':'issueDisabled'} onClick={this.handleSubmit}>
+						<p>Submit</p>
+					</div>
+				</div>
+			);
+		}
 	}
 }
