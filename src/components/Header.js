91db306472b1a8a91d8ae2a7602dc1e0ab7e91ef
Ryan Zimmerman <w.ryan.zimmerman@gmail.com>
2019-07-14 23:37:02 +0800

    Simplified Navbar Structure
    Simplified navbar structure for accessability and maintainability. Replaced custom MenuLink with built-in NavLink, and implemented active style using the aria-current attribute created by NavLink. Restyled the navbar to look like tabs, not great but serviceable for now. The Download CSV button should be moved, once it's moved the tab bar can be floated to the right (as it is, the download button would move the entire tab bar back and forth).

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MenuLink from './MenuLink';
+import { NavLink } from 'react-router-dom';
 import Conditional from './Conditional';
 import { apiPath } from '../config';
 
@@ -8,19 +8,19 @@ import './Header.css';
 const Header = (props) => {
 	return(
 		<div className='header'>
-			<MenuLink link='/form/item' highlight={props.view==='form'&&props.type==='item'}>New Item</MenuLink>
-			<MenuLink link='/form/book' highlight={props.view==='form'&&props.type==='book'}>New Book</MenuLink>
-			<MenuLink link='/list/items' highlight={props.view==='list'&&props.type==='items'}>All Items</MenuLink>
-			<MenuLink link='/list/books' highlight={props.view==='list'&&props.type==='books'}>Books</MenuLink>
-			<MenuLink link='/data' highlight={props.view==='data'}>Saved Data</MenuLink>
-			<MenuLink link='/support' highlight={props.view==='support'}>Support</MenuLink>
-			<Conditional render={props.type && props.view !== 'form'}>
-				<a href={`${apiPath}/${props.type}/csv/`}>
-					<div className='externalLinkContainer'>
-						<p>Download CSV</p>
-					</div>
-				</a>
-			</Conditional>
+			<ul id='nav'>
+				<li><NavLink to='/form/item'>New Item</NavLink></li>
+				<li><NavLink to='/form/book'>New Book</NavLink></li>
+				<li><NavLink to='/list/items'>All Items</NavLink></li>
+				<li><NavLink to='/list/books'>Books</NavLink></li>
+				<li><NavLink to='/data'>Saved Data</NavLink></li>
+				<li><NavLink to='/support'>Support</NavLink></li>
+				{/* The Download CSV button should be moved elsewhere */}
+				{/* It's really bad UX to have this look like a tab, I'm sorry */}
+				<Conditional render={props.type && props.view !== 'form'}>
+					<li><a href={`${apiPath}/${props.type}/csv/`}>Download CSV</a></li>
+				</Conditional>
+			</ul>
 		</div>
 	);
 }
